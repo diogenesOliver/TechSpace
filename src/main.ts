d64f48806cd3e1eb94d39c9c751fdf38f5f75aa7
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-11-13 16:55:34 -0200

    File router created

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,6 @@ import { Request, Response } from 'express'
 const app = express()
 const PORT = 8080
 
-app.get('/', (req: Request, res: Response) => {
-
-    res.send('Testando')
-
-})
-
 app.listen(8080, () => {
     console.log(`Server run on port: ${PORT}`)
 })
