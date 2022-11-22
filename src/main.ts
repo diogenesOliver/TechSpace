9351d0f69b1baf1f3c77b6aa4a9bf573c0d30c29
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-11-22 20:05:46 +0000

    Removing all

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { Request, Response } from 'express'
-
-const app = express()
-const PORT = 8080
-
-app.listen(8080, () => {
-    console.log(`Server run on port: ${PORT}`)
-})
